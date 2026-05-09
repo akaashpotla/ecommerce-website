975c7b875467f143ed72de8086e0d571c81beb7d
akaashpotla1 <akaash@tntgamez.com>
2026-05-09 09:56:57 -0500

    Added auth route and started frontend

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import Depends, FastAPI
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from app.api.v1.users import router as users_router
+from app.api.v1.auth import router as auth_router
 from app.db.session import get_db
 
 app = FastAPI()
 
 app.include_router(users_router)
+app.include_router(auth_router)
 
 @app.get("/health")
 def health():
---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
